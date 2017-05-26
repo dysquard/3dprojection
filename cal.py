5a76169d8165df9c57ca491ea0f0850efa4506ad
dysquard <99404140@qq.com>
2017-05-26 23:51:59 +0800

    projection transformation

---
+++ cal.py
@@ -0,0 +1,23 @@
+from numpy import *
+from math import *
+
+def cal():
+     fov = 45 * math.pi /180
+     aspect = 16/9
+     n = 66.61
+     f = 160
+     A = mat(([-40],[8.977],[-114.269],[1]))
+     B = mat(([-38.903],[5.287],[-106.719],[1]))
+     P = mat(([1/aspect*math.tan(fov),0,0,0],[0,1/math.tan(fov),0,0],[0,0,f/(f-n),1],[0,0,f*n/(n-f),0]))
+#     P = mat(([1/(aspect*math.tan(fov/2)),0,0,0],[0,1/math.tan(fov/2),0,0],[0,0,(f+n)/(n-f),2*f*n/(n-f)],[0,0,-1,0]))
+
+     a = P*A
+     b = P*B
+
+     a = a/a[3]
+     b = b/b[3]
+  
+
+     print('a=',a)
+     print('b=',b)
+
