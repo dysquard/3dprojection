7464b6503c1db7597afee0b3a7d34d2b2f254e8f
dysquard <99404104@qq.com>
2017-05-27 17:58:47 +0800

    add g to G

--- cal.py
+++ cal.py
@@ -1,10 +1,10 @@
 from numpy import *
 from math import *
 
-fov = 45 * math.pi /180
+fov = 37.97 * math.pi /180
 aspect = 16/9
-n = 66.61
-f = 160
+n = 1
+f = 30
 A = mat(([-40],[8.977],[-114.269],[1]))
 B = mat(([-38.903],[5.287],[-106.719],[1]))
 P = mat(([1/aspect*math.tan(fov),0,0,0],[0,1/math.tan(fov),0,0],[0,0,f/(f-n),1],[0,0,f*n/(n-f),0]))
@@ -12,6 +12,7 @@ P = mat(([1/aspect*math.tan(fov),0,0,0],[0,1/math.tan(fov),0,0],[0,0,f/(f-n),1],
 
 # a represents the point after perspective transformation
 # A represents the point before perspective transformation
+# a & A are at the same coordinate system (eye/camera)
      
 a = P*A
 #A = P.I * a
@@ -23,4 +24,21 @@ b = b/b[3]
 
 print('a=',a)
 print('b=',b)
+
+
+
+#___________________________
+
+g = mat(([100],[1000],[1],[1])) #g is the coordinate that you GET
+
+G = P.I * g
+
+r = 2.871
+
+YG = float(G[2])
+ZG = YG/math.tan(fov) - 2*r
+
+O = mat(([0],[-0.059*math.cos(fov)],[-2*r+0.059*math.sin(fov)]))
+
+T = mat(([0],[-0.059*math.cos(fov)+r*math.sin(fov)],[-2*r+0.059*math.sin(fov)-r*math.cos(fov)]))
 
